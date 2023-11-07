d3da09cc4d9c15eeebcdc60800c10193d44c646b
unknown <sakibbangla49@gmail.com>
2023-11-07 02:42:08 -0800

    get food data

--- index.js
+++ index.js
@@ -42,6 +42,11 @@ async function run() {
       const result = await foodCollection.insertOne(addFood);
       res.send(result)
     })
+    app.get('/food', async(req,res)=>{
+      const cursor = foodCollection.find();
+      const result= await cursor.toArray();
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
