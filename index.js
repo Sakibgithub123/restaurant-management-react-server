c4cc398f0b1d9f4d11c9ac8402af8be9d2ea8558
unknown <sakibbangla49@gmail.com>
2023-11-08 02:25:46 -0800

    delete user order

--- index.js
+++ index.js
@@ -70,6 +70,12 @@ async function run() {
       const result= await cursor.toArray();
       res.send(result)
     })
+    app.delete('/myorderfood/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id: new ObjectId(id)}
+      const result = await myOrderCollection.deleteOne(query);
+      res.send(result)
+    })
     
 
     //myorderend
